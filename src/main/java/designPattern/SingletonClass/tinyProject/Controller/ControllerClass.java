ba85347b198f25813a6009ece688b9d805980327
SurwaseAbhishek <surwaseabhishek4431@gmail.com>
2025-07-08 23:31:47 +0530

    Realtime Singleton we can create this as servlet service and dao

---
+++ src/main/java/designPattern/SingletonClass/tinyProject/Controller/ControllerClass.java
@@ -0,0 +1,38 @@
+package designPattern.SingletonClass.tinyProject.Controller;
+
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.management.InstanceAlreadyExistsException;
+
+import designPattern.SingletonClass.tinyProject.service.ServiceClass;
+
+public class ControllerClass {
+	public static void main(String[] args) throws InstanceAlreadyExistsException {
+		ServiceClass service = ServiceClass.getInstance();
+		Hashtable<String, List<String>> contriesList = service.getCountriesList();
+		System.out.println(contriesList.get("cInfo"));
+		experimentMethod1();
+		experimentMethod2();
+	}
+
+	private static void experimentMethod2() throws InstanceAlreadyExistsException {
+		ServiceClass service = ServiceClass.getInstance();
+		Hashtable<String, List<String>> contriesList = service.getCountriesList();
+		System.out.println(contriesList.get("cInfo"));		
+	}
+
+	private static void experimentMethod1() throws InstanceAlreadyExistsException {
+		ServiceClass service = ServiceClass.getInstance();
+		Hashtable<String, List<String>> contriesList = service.getCountriesList();
+		System.out.println(contriesList.get("cInfo"));
+	}
+	
+	
+	/*
+	 * Most of the time if u are thinking to create Singleton class at that time
+	 * utility class we can create it doesont means every time utility class can
+	 * replace singleton everyone is perfect at there own position Utility class is
+	 * class which is having all methods as static method
+	 */
+}
---
+++ src/main/java/designPattern/SingletonClass/tinyProject/service/ServiceClass.java
@@ -0,0 +1,52 @@
+package designPattern.SingletonClass.tinyProject.service;
+
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.management.InstanceAlreadyExistsException;
+
+import designPattern.SingletonClass.tinyProject.dao.daoClass;
+
+public class ServiceClass  {
+	private static ServiceClass SERVICE;
+	private Hashtable<String, List<String>> Countries; ;
+	boolean isIstanciated;
+	public ServiceClass() throws InstanceAlreadyExistsException {
+		if(isIstanciated) {
+			throw new InstanceAlreadyExistsException();
+		}
+		isIstanciated = false;
+		Countries = new Hashtable<String, List<String>>();
+	}
+	
+	public static ServiceClass getInstance() throws InstanceAlreadyExistsException {
+		if(SERVICE == null) {
+			synchronized (ServiceClass.class) {
+				if(SERVICE==null) {
+					SERVICE = new ServiceClass();
+				}
+			}
+		}
+		return SERVICE;
+	}
+	
+	public Object readResolve() {
+		return SERVICE;
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return SERVICE;
+	}
+	
+	// business Logic 
+	public Hashtable getCountriesList() {
+		if(!Countries.containsKey("cInfo")) {
+			daoClass dao = new daoClass();
+			List<String> counties = dao.contriesList;
+			Countries.put("cInfo", counties);
+		}
+		return Countries;
+		
+	}
+}
